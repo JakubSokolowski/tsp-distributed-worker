315724c5c9a35161c0adb7eae831195ea77b3be1
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-04-17 23:27:08 +0200

    Add google's protobuf library to project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "proto/message.pb.h"
+#include <ostream>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    message::Message m;
+    m.set_id(0,1);
+    std::cout << "Hello Proto!"  << std::endl;
     return 0;
 }
